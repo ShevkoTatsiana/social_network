89a1cf0ddf815e17e48977900fc583bce6dc650a
Tatsiana Sheuka <Tatsiana.Sheuka@itechart-group.com>
2022-04-29 18:13:26 +0300

    add FE form validation

--- app/src/components/LoginForm.component.js
+++ app/src/components/LoginForm.component.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { useForm } from 'react-hook-form'; 
 import Alert from 'react-bootstrap/Alert';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel'
+import Button from 'react-bootstrap/esm/Button';
 
 export const LoginFormComponent = ({error, onFormSubmit}) => {
  
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = (data) => {
     onFormSubmit(data)
   };
@@ -15,13 +18,35 @@ export const LoginFormComponent = ({error, onFormSubmit}) => {
                    variant="danger">   
                 {error?.message}
             </Alert> 
-      <form  onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="email">Email</label>
-        <input {...register("email")} id="email"/>
-        <label htmlFor="password">Password</label>
-        <input {...register("password")} id="password"/>
-        <input type="submit" />
-      </form>
+      <Form noValidate onSubmit={handleSubmit(onSubmit)}
+                       className="form-component">
+      <FloatingLabel label="User Email">
+          <Form.Control placeholder="User Email"
+                        isInvalid={errors?.email}
+                        {...register("email", {
+                          required:{value: true, message: "This is a required field"}
+                        })}/>
+          <Form.Control.Feedback type="invalid">
+            {errors?.email?.message}
+          </Form.Control.Feedback>
+        </FloatingLabel>
+        <FloatingLabel label="Password">
+          <Form.Control placeholder="Password"
+                        isInvalid={errors?.password}
+                        {...register("password", {
+                          required: {value: true, message: "This is a required field"},
+                          pattern: {value: /^[a-zA-Z0-9]{3,30}$/, message: "Invalid password"}
+                        })} />
+          <Form.Control.Feedback type="invalid">
+            {errors?.password?.message}
+          </Form.Control.Feedback>
+        </FloatingLabel>
+        <Button variant="primary" 
+                type="submit"
+                className="form-submit">
+          Login
+        </Button>
+      </Form>
     </div>
   );
 }
--- app/src/components/UserForm.component.js
+++ app/src/components/UserForm.component.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
-import { useForm } from 'react-hook-form'; 
+import { useForm, useFormState } from 'react-hook-form'; 
 import Alert from 'react-bootstrap/Alert';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel'
+import Button from 'react-bootstrap/esm/Button';
 
 export const UserFormComponent = (props) => {
   const {
@@ -11,7 +14,7 @@ export const UserFormComponent = (props) => {
     onFormSubmit
   } = props;
 
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const [profilePhoto, setProfilePhoto] = useState('');
   const onSubmit = (data) => {
     const formData = new FormData();
@@ -33,25 +36,62 @@ export const UserFormComponent = (props) => {
         {error.message}
       </Alert> 
       )}       
-      <form  onSubmit={handleSubmit(onSubmit)}
-             encType="multipart/form-data">
-        <label htmlFor="name">Name</label>
-        <input defaultValue={name}
-              {...register("name")} id="name"/>
-        <label htmlFor="email">Email</label>
-        <input defaultValue={email}
-              {...register("email")} id="email"/>
-        <label htmlFor="password">Password</label>
-        <input defaultValue={password}
-              {...register("password")} id="password"/>
-        <label htmlFor="profile_photo">Upload your Profile Photo here</label>
-        <input type="file" 
+      <form noValidate onSubmit={handleSubmit(onSubmit)}
+             encType="multipart/form-data"
+             className="form-component">
+        <FloatingLabel label="User Name">
+          <Form.Control defaultValue={name}
+                        placeholder="User Name"
+                        isInvalid={errors?.name} 
+                        {...register("name", {
+                          required: {value: true, message: "This is a required field"},
+                          minLength: {value: 3, message: "Min length is 3"}
+                        })} />
+            <Form.Control.Feedback type="invalid">
+              {errors?.name?.message}
+          </Form.Control.Feedback>
+        </FloatingLabel>
+        
+        <FloatingLabel label="User Email">
+          <Form.Control defaultValue={email}
+                        placeholder="User Email"
+                        isInvalid={errors?.email}
+                        {...register("email", {
+                          required:{value: true, message: "This is a required field"}
+                        })}/>
+          <Form.Control.Feedback type="invalid">
+            {errors?.email?.message}
+          </Form.Control.Feedback>
+        </FloatingLabel>
+       
+        <FloatingLabel label="Password">
+          <Form.Control defaultValue={password}
+                        placeholder="Password"
+                        isInvalid={errors?.password}
+                        {...register("password", {
+                          required: {value: true, message: "This is a required field"},
+                          pattern: {value: /^[a-zA-Z0-9]{3,30}$/, message: "Invalid password"}
+                        })} />
+          <Form.Control.Feedback type="invalid">
+            {errors?.password?.message}
+          </Form.Control.Feedback>
+        </FloatingLabel>
+        <Form.Label htmlFor="profile_photo">Upload your Profile Photo here</Form.Label>
+        <Form.Control type="file" 
                className="user-form-component-photo" 
                name="profile_photo"
                {...register("profile_photo")} 
-               id="profile_photo"
+             
                onChange={onFileUpload}/>
-        <input type="submit" />
+        <Form.Text id="filesHelpBlock" muted
+                   className="form-note">
+          You can upload '.jpg', '.jpeg' or '.png' files here.
+        </Form.Text>
+        <Button variant="primary" 
+                type="submit"
+                className="form-submit">
+          Submit
+        </Button>
       </form>
     </div>
   );
