064b0e2269ad4f000d0d28c9104bf60201e7585a
Tatsiana Sheuka <Tatsiana.Sheuka@itechart-group.com>
2022-05-27 10:32:27 +0300

    rework to heroku deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import path from 'path';
 import {rootRouter} from './routing.js';
 import { config } from './config.js';
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "app", "build")));
 
 mongoose.connect(config.uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
@@ -20,7 +22,9 @@ app.use('/images', express.static('./images'));
 app.use(express.urlencoded({ extended: true }));
 app.use(rootRouter);
 
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "app", "build", "index.html"));
+});
 app.listen(config.port, () => {
   console.log(`Server is running at port ${config.port}`);
 });
